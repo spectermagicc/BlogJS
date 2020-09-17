f8c16bc4357eb41ea9901cdec3da7c8b5b393350
spectermagicc <69127196+spectermagicc@users.noreply.github.com>
2020-09-17 00:12:15 -0300

    Add files via upload

--- envio.php
+++ envio.php
@@ -1,10 +1,13 @@
 <?php
 
-//Lamar a los campos del formutario
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
+/*
 
+//Lamar a los campos del formutario
+$nombre = $_GET['nombre'];
+$correo = $_GET['correo'];
+$mensaje = $_GET['mensaje'];
+
+echo "iNFORMACION: $nombre, $correo, $mensaje";
 //Datos requeridos para el correo
 $destinatario = "maigrojas.dev@gmail.com"; //correo al que vamos a enviar
 $asunto = "Contacto desde Blog de JavaScript";
@@ -16,6 +19,17 @@ $contenido .= "Mensaje: $mensaje";
 
 //Enviando mensaje
 mail($destinatario, $asunto, $contenido);
-header('Location: mensajeEnviado.html');
+header('Location: mensajeEnviado.html');*/
+
+$destino = "maigrojas.dev@gmail.com";
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+
+mail($destino, "Contacto", $contenido);
+header("Location: mensajeEnviado.html");
 
 ?>
